c6442f82578509617f1d466f3bc19a87a48eae75
guo602 <370040709@qq.com>
2026-03-25 14:04:22 +0800

    Initial Commit

---
+++ rgzzhangTests/rgzzhangTests.swift
@@ -0,0 +1,17 @@
+//
+//  rgzzhangTests.swift
+//  rgzzhangTests
+//
+//  Created by hamburger on 2026/3/25.
+//
+
+import Testing
+@testable import rgzzhang
+
+struct rgzzhangTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
